e784e75996d636f1cc9942238eb975003da08656
PC2 <annydemelozebulum@gmail.com>
2024-02-26 00:28:36 +0100

    commit

--- Session #3&4 pt.2.py
+++ Session #3&4 pt.2.py
@@ -7,3 +7,17 @@ print(c)
 speed = 35.0
 time = 43
 distance = speed * time
+print(distance)
+
+#What does the next code print?
+a = 6
+print(a + 2.0)
+a /= 2
+print(a)
+b = 3
+print(a**b)
+b -= a
+print(b + 2)
+c = a > b
+print(c and a)
+print(a + b + c)
